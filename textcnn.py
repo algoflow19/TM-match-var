c72fccdb7772a4c853bb984afe3a90a915f1ceb9
developerkk <2957691307@qq.com>
2019-10-19 07:51:36 +0800

    Fix

--- textcnn.py
+++ textcnn.py
@@ -25,7 +25,7 @@ class textCNN(nn.Module):
     self.embedding_size=embedding_size
     self.cls_num=cls_num
     self.lastScore=0
-    self.l1_convs=nn.ModuleList([torch.relu(nn.Conv1d(embedding_size,l1_channels_num,i)) for i in self.filter_sizes] )
+    self.l1_convs=nn.ModuleList([nn.Conv1d(embedding_size,l1_channels_num,i) for i in self.filter_sizes] )
     self.pools=nn.ModuleList([nn.MaxPool1d(i[0],stride=i[1]) for i in self.pool_sizes])
     self.l2_convs=nn.ModuleList([nn.Conv1d(l1_channels_num,l2_channels_num,i) for i in self.filter_sizes2])
     self.dropout = nn.Dropout(dropout_rate)
@@ -39,7 +39,7 @@ class textCNN(nn.Module):
       input_tensor: shape(batch,maxLenSen,embed_size)
     """
     input_tensor=input_tensor.permute(0,2,1).contiguous()
-    l1_outs=[conv(input_tensor) for conv in self.l1_convs]
+    l1_outs=[torch.relu(conv(input_tensor)) for conv in self.l1_convs]
     l2_outs=[  self.l2_convs[i](self.pools[i](l1_outs[i]))  for i in range(len(self.filter_sizes2))]
     l2_outs=[torch.max(out,2)[0] for out in l2_outs]
     l2_outs=self.dropout(torch.cat(l2_outs,1))
